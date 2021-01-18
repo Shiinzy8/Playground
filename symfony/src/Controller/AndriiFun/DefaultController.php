4f0f7e00eceb94658490df66a38dbf11449ed9c6
Andrii <Shiinzy8@gmail.com>
2021-01-18 18:15:08 +0200

    Use annotations for the routing

--- symfony/src/Controller/AndriiFun/DefaultController.php
+++ symfony/src/Controller/AndriiFun/DefaultController.php
@@ -3,11 +3,26 @@
 namespace App\Controller\AndriiFun;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController
 {
+    /**
+     * @Route ("/")
+     * @return Response
+     */
     public function index(): Response
     {
-        return new Response("Hello wolrd");
+        return new Response("Hello world");
+    }
+
+    /**
+     * @Route ("/show/{id}")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        return new Response("Future page to show something with id: {$id}");
     }
 }
