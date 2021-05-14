cf8fb2c0f87fb262794fd9123a3ce3bbf0eb5d5b
Andrii <Shiinzy8@gmail.com>
2021-05-14 20:37:27 +0300

    long way to bound two events click and reaction of another component for this click

--- symfony/andrii/mvp_office/assets/js/components/sidebar.vue
+++ symfony/andrii/mvp_office/assets/js/components/sidebar.vue
@@ -30,7 +30,7 @@
         <div class="d-flex justify-content-end">
             <button
                 class="btn btn-secondary btn-sm"
-                @click="toggleCollapsed"
+                @click="$emit('toggle-collapsed')"
                 v-text="collapsed ? '>>' : '<< Collapse'"
             />
         </div>
@@ -73,12 +73,6 @@ export default {
                 classes.push(this.$style.collapsed);
             }
             return classes;
-        },
-    },
-    methods: {
-        toggleCollapsed() {
-            // console.log('CLICKED!');
-            this.$emit('toggle-collapsed');
         },
     },
 };
