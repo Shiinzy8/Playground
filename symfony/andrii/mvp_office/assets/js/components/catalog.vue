e15b18dc16aa79471031147a44f3b18177e6ce56
Andrii <Shiinzy8@gmail.com>
2021-05-25 16:22:07 +0300

    add working search filter for products

--- symfony/andrii/mvp_office/assets/js/components/catalog.vue
+++ symfony/andrii/mvp_office/assets/js/components/catalog.vue
@@ -8,11 +8,13 @@
                 />
             </div>
             <div class="col-9">
-                <search-bar />
+                <search-bar
+                    @search-products="onSearchProducts"
+                />
             </div>
         </div>
         <product-list
-            :products="products"
+            :products="filteredProducts"
             :loading="loading"
         />
         <div class="row">
@@ -55,6 +57,17 @@ export default {
             legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
         };
     },
+    computed: {
+        filteredProducts() {
+            if (!this.searchTerm) {
+                return this.products;
+            }
+
+            return this.products.filter((product) => (
+                product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+            ));
+        },
+    },
     // it is also can be created function
     // async mounted() {
     async created() {
@@ -79,6 +92,11 @@ export default {
 
         // console.log(response);
         this.products = response.data['hydra:member'];
+    },
+    methods: {
+        onSearchProducts(event) {
+            this.searchTerm = event.term;
+        },
     },
 };
 </script>
